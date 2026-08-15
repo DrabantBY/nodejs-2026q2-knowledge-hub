baead705bba4e7d6939ce668b93e473781f5935a
DrabantBY <yauheni.basalai@gmail.com>
2026-08-15 15:09:36 +0300

    feat: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { ArticlesModule, CategoriesModule, CommentsModule, UsersModule } from './modules';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    JwtModule.register({ global: true }),
+    UsersModule,
+    ArticlesModule,
+    CategoriesModule,
+    CommentsModule,
+  ],
 })
 export class AppModule {}
---
+++ src/modules/index.ts
@@ -0,0 +1,4 @@
+export * from './articles/articles.module';
+export * from './categories/categories.module';
+export * from './comments/comments.module';
+export * from './users/users.module';
