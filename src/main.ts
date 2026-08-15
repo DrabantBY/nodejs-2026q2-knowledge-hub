feb29f528ef31109234b80d08448d4ad710b1677
DrabantBY <yauheni.basalai@gmail.com>
2026-08-15 15:57:50 +0300

    feat: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app);
   await app.listen(Number(process.env.PORT) ?? 4000);
 }
 bootstrap();
