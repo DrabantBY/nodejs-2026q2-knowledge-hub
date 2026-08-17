eef8d8d1c56805947c4fb6eb448d106fbeed6b60
DrabantBY <yauheni.basalai@gmail.com>
2026-08-18 00:27:29 +0300

    feat: update env

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,10 @@
 import 'dotenv/config';
 import { defineConfig, env } from 'prisma/config';
 
+if (!process.env.DATABASE_URL) {
+  process.env.DATABASE_URL = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}?schema=public`;
+}
+
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './swagger';
 
+if (!process.env.DATABASE_URL) {
+  process.env.DATABASE_URL = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}?schema=public`;
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setupSwagger(app);
